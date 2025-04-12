bd7afa47db8a4b7d0b829380802bef73f7976d14
Axel Nana <axel.nana@workbud.com>
2025-04-12 02:20:19 +0100

    feat(app): Added lifecycle hooks.

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,10 +1,11 @@
-import type { AnyElysia, ElysiaConfig } from 'elysia';
+import type { AnyElysia, ElysiaConfig, ErrorContext } from 'elysia';
 import type { Class } from 'type-fest';
 import type { Module } from './module';
 import type { Route } from './utils';
 
 import { Elysia } from 'elysia';
 
+import { Event } from './event';
 import { Service } from './service';
 import { Symbols } from './utils';
 
@@ -34,14 +35,39 @@ export const app = (props: AppProps): ClassDecorator => {
  * @author Axel Nana <axel.nana@workbud.com>
  */
 export abstract class Application {
-	#elysia: AnyElysia;
+	#elysia: Elysia<Route>;
+	#debug: boolean;
 
 	/**
 	 * Creates a new instance of the application.
 	 * @param config The Elysia configuration.
 	 */
-	constructor(config: ElysiaConfig<Route> = {}) {
+	constructor(config: ElysiaConfig<Route> & { debug?: boolean } = {}) {
 		this.#elysia = new Elysia(config);
+		this.#debug = config.debug ?? false;
+
+		this.#elysia.onRequest((c) => {
+			if (this.debug) {
+				// TODO: Use the logger service here
+				console.log(c.request.method, c.request.url);
+			}
+		});
+
+		this.#elysia.onError((e) => {
+			if (this.onError(e)) {
+				Event.emit('elysium:error', e);
+			}
+		});
+
+		this.#elysia.onStart((c) => {
+			this.onStart(c);
+			Event.emit('elysium:app:start', c);
+		});
+
+		this.#elysia.onStop((c) => {
+			this.onStop(c);
+			Event.emit('elysium:app:stop', c);
+		});
 	}
 
 	/**
@@ -50,6 +76,42 @@ export abstract class Application {
 	public get elysia(): AnyElysia {
 		return this.#elysia;
 	}
+
+	/**
+	 * Gets the debug mode flag.
+	 */
+	public get debug(): boolean {
+		return this.#debug;
+	}
+
+	/**
+	 * Sets the debug mode flag.
+	 * @param debug The debug mode flag.
+	 */
+	public set debug(debug: boolean) {
+		this.#debug = debug;
+	}
+
+	/**
+	 * Hook that is executed when an error occurs.
+	 * @param e The error context.
+	 * @returns Whether to continue the error propagation.
+	 */
+	public onError(e: ErrorContext): boolean {
+		return true;
+	}
+
+	/**
+	 * Hook that is executed when the application starts.
+	 * @param elysia The Elysia instance.
+	 */
+	public onStart(elysia: Elysia<Route>): void {}
+
+	/**
+	 * Hook that is executed when the application stops.
+	 * @param elysia The Elysia instance.
+	 */
+	public onStop(elysia: Elysia<Route>): void {}
 
 	/**
 	 * Starts the application on the specified port.
@@ -71,7 +133,7 @@ export abstract class Application {
 			const module = Service.bind(moduleClass);
 
 			module.beforeRegister();
-			this.#elysia = this.#elysia.use(await plugin(module));
+			this.#elysia.use(await plugin(module));
 			module.afterRegister();
 		}
 
